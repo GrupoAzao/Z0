324d8a36bf0aae0d585d2535f99547c0e5692e47
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-03 10:11:06 -0300

    fiz uma leve alteração para ver se o barrel8 funciona

--- Computador/Modulos/PortasLogicas/BarrelShifter8.vhd
+++ Computador/Modulos/PortasLogicas/BarrelShifter8.vhd
@@ -10,15 +10,15 @@ entity BarrelShifter8 is
 end entity;
 architecture arch_BarrelShifter8 of BarrelShifter8 is
 begin 
-	q <= a when dir = '0' and size = "000" else
-		  a(7 downto 1) & a(0) when dir = '0' and size = "001" else
-		  a(7 downto 2) & a(1 downto 0) when dir = '0' and size = "010" else
-	     a(7 downto 3) & a(2 downto 0) when dir = '0' and size = "011" else
-		  a(7 downto 4) & a(3 downto 0) when dir = '0' and size = "100" else
+	q <= a when (dir = '0' and size = "000") else
+		  a(7 downto 1) & a(0) when (dir = '0' and size = "001") else
+		  a(7 downto 2) & a(1 downto 0) when (dir = '0' and size = "010") else
+	     a(7 downto 3) & a(2 downto 0) when (dir = '0' and size = "011") else
+		  a(7 downto 4) & a(3 downto 0) when (dir = '0' and size = "100") else
 		  a when dir = '1' and size = "000" else
-		  a(7) & a(6 downto 0)  when dir = '1' and size = "001" else
-		  a(7 downto 6) & a(5 downto 0) when dir = '1' and size = "010" else
-	     a(7 downto 5) & a(4 downto 0)  when dir = '1' and size = "011" else
-	     a(7 downto 4) & a(3 downto 0)  when dir = '1' and size = "100";
+		  a(7) & a(6 downto 0)  when (dir = '1' and size = "001") else
+		  a(7 downto 6) & a(5 downto 0) when (dir = '1' and size = "010") else
+	     a(7 downto 5) & a(4 downto 0)  when (dir = '1' and size = "011") else
+	     a(7 downto 4) & a(3 downto 0)  when (dir = '1' and size = "100");
 	
 end architecture;
