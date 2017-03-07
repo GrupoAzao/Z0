4111dc14229a7174a383e911d28f3a611d765ad3
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-07 16:15:59 -0300

    concertei o barrelshifter16

--- Computador/Modulos/PortasLogicas/BarrelShifter16.vhd
+++ Computador/Modulos/PortasLogicas/BarrelShifter16.vhd
@@ -8,3 +8,16 @@ entity BarrelShifter16 is
 			size: in  std_logic_vector(2 downto 0);    -- shift amount
 			q:    out STD_LOGIC_VECTOR(15 downto 0));  -- output vector (shifted)
 end entity;
+architecture arch_BarrelShifter16 of BarrelShifter16 is
+begin 
+	q <= a when (size = "000") else
+		  a(0) & a(15 downto 1) when (dir = '1' and size = "001") else
+		  a(1 downto 0) &  a(15 downto 2) when (dir = '1' and size = "010") else
+	     a(2 downto 0) & a(15 downto 3) when (dir = '1' and size = "011") else
+		  a(3 downto 0) & a(15 downto 4) when (dir = '1' and size = "100") else
+		  a(14 downto 0) & a(15)  when (dir = '0' and size = "001") else
+		  a(13 downto 0) & a(15 downto 14) when (dir = '0' and size = "010") else
+	     a(12 downto 0) & a(15 downto 13) when (dir = '0' and size = "011") else
+	     a(11 downto 0) & a(15 downto 12) when (dir = '0' and size = "100");
+	
+end architecture;
