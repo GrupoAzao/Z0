eda7dba9e3f7c0a63770c4d2c02b72425473e6d1
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-07 21:52:44 -0300

    mux8way16 funcionando

--- Computador/Modulos/PortasLogicas/Mux8Way16.vhd
+++ Computador/Modulos/PortasLogicas/Mux8Way16.vhd
@@ -14,3 +14,15 @@ entity Mux8Way16 is
 			sel: in  STD_LOGIC_VECTOR(2 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+architecture arch_mux8way16 of Mux8way16 is
+begin
+	q <= a when (sel = "000") else
+		  b when (sel= "001") else
+		  c when (sel = "010") else
+		  d when (sel ="011") else 
+		  e when (sel="100") else
+		  f when (sel="101") else
+		  g when (sel="110") else
+		  h when (sel="111");
+end architecture;
+	
