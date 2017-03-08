4bec0d62d15de3770f4cfcd5a70e79e589ebe991
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-08 11:16:51 -0300

    adicionando o full adder e o half adder que o tirta fez

--- Computador/Modulos/UnidadeLogicaAritmetica/FullAdder.vhd
+++ Computador/Modulos/UnidadeLogicaAritmetica/FullAdder.vhd
@@ -9,3 +9,12 @@ entity FullAdder is
 		soma,vaium: out STD_LOGIC   -- sum e carry
 	);
 end entity;
+architecture arch_FullAdder of FullAdder is 
+signal w1, w2, w3 : STD_LOGIC;
+begin
+	w1 <= a xor b;
+	w2 <= a and b;
+	w3 <=w1 and c;
+	soma <= w1 xor c;
+	vaium	 <= w3 or w2;
+end architecture;
