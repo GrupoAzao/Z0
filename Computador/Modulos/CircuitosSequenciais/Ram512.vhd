b577642b546738ee68928a5407053958c0b19034
vilela69 <frederico.vilelacurti@gmail.com>
2017-03-21 15:31:49 -0300

    adicionada ram512

--- Computador/Modulos/CircuitosSequenciais/Ram512.vhd
+++ Computador/Modulos/CircuitosSequenciais/Ram512.vhd
@@ -2,15 +2,31 @@
 -- by Luciano Soares
 -- Ram512.vhd
 
-Library ieee; 
+Library ieee;
 use ieee.std_logic_1164.all;
-  
+USE ieee.numeric_std.ALL;
+
 entity Ram512 is
 	port(
 		clock:   in  STD_LOGIC;
 		input:   in  STD_LOGIC_VECTOR(15 downto 0);
 		load:    in  STD_LOGIC;
-		address: in  STD_LOGIC_VECTOR( 8 downto 0);
+		address: in  STD_LOGIC_VECTOR(8 downto 0);
 		output:  out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
+
+ARCHITECTURE arch_ram512 OF Ram512 IS
+   TYPE mem IS ARRAY(0 TO 511) OF std_logic_vector(15 DOWNTO 0);
+   SIGNAL ram_block : mem;
+BEGIN
+   PROCESS (clock)
+   BEGIN
+      IF (clock'event AND clock = '1') THEN
+         IF (load = '1') THEN
+            ram_block(to_integer(unsigned(address))) <= input;
+         END IF;
+      END IF;
+   END PROCESS;
+   output <= ram_block(to_integer(unsigned(address)));
+END arch_ram512;
