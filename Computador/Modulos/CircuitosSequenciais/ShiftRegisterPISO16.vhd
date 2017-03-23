79f65ce6b3d039a2fd4712572c75af45cdcd7eff
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-23 04:02:14 -0300

    concertei um erro de nomeação

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
@@ -1,41 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity ShifterRegisterPISO16 is
+entity ShiftRegisterPISO16 is
     Port ( clock :  in   STD_LOGIC;
            shift : in   STD_LOGIC;
            input :    in   STD_LOGIC_VECTOR (15 downto 0);
            output :    out  STD_LOGIC);
 end entity;
 
-architecture arch_piso of ShifterRegisterPISO16 is
-
-signal sr: std_logic_vector(15 downto 0) := (others=>'0');
-
-begin
-
-  process (clock,shift,input)
-  begin
-    if (shift = '0') then
-      sr <= input;
-    elsif rising_edge(clock) then
-      sr <= '0' & sr(15 downto 1);
-    end if;
-  end process;
-
-  output <= sr(0);      
- 
-end architecture;library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity ShifterRegisterPISO16 is
-    Port ( clock :  in   STD_LOGIC;
-           shift : in   STD_LOGIC;
-           input :    in   STD_LOGIC_VECTOR (15 downto 0);
-           output :    out  STD_LOGIC);
-end entity;
-
-architecture arch_piso of ShifterRegisterPISO16 is
+architecture arch_piso of ShiftRegisterPISO16 is
 
 signal sr: std_logic_vector(15 downto 0) := (others=>'0');
 
