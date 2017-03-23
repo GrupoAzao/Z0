b884ec9bc7007c9f01b06511398f59b7462f55f2
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-23 01:40:46 -0300

    mais um fucking teste

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
@@ -3,25 +3,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ShiftRegisterPISO16 is
     Port ( clock :  in   STD_LOGIC;
-           shift: in   STD_LOGIC;
+           shift : in   STD_LOGIC;
            input :    in   STD_LOGIC_VECTOR (15 downto 0);
            output :    out  STD_LOGIC);
-end entity;
+end Piso;
 
-architecture arch_piso of ShiftRegisterPISO16 is
+architecture Behavioral of Piso is
 
-  signal w1: std_logic_vector(15 downto 0) := (others=>'0');
+  signal sr: std_logic_vector(15 downto 0) := (others=>'0');
 
 begin
 
   process (clock,shift,input)
   begin
     if (shift = '1') then
-      w1 <= input;
+      sr <= input;
     elsif rising_edge(clock) then
-      w1 <= '0' & w1(15 downto 1);
+      sr <= '0' & sr(15 downto 1);
     end if;
   end process;
-  output <= w1(0);      
+
+  output <= sr(0);      
  
-end architecture;
+end Behavioral;
