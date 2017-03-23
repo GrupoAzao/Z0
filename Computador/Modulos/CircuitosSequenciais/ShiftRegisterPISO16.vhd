28cc3ca13524d5e9ebd71d0d6b8b0f8fc3c69ff4
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-23 03:46:40 -0300

    okay agora sim é o ultimo teste do PISO16

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
@@ -6,11 +6,11 @@ entity ShifterRegisterPISO16 is
            shift : in   STD_LOGIC;
            input :    in   STD_LOGIC_VECTOR (15 downto 0);
            output :    out  STD_LOGIC);
-end ShifterRegisterPISO16;
+end entity;
 
-architecture Behavioral of ShifterRegisterPISO16 is
+architecture arch_piso of ShifterRegisterPISO16 is
 
-  signal sr: std_logic_vector(15 downto 0) := (others=>'0');
+signal sr: std_logic_vector(15 downto 0) := (others=>'0');
 
 begin
 
@@ -25,4 +25,31 @@ begin
 
   output <= sr(0);      
  
-end Behavioral;
+end architecture;library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ShifterRegisterPISO16 is
+    Port ( clock :  in   STD_LOGIC;
+           shift : in   STD_LOGIC;
+           input :    in   STD_LOGIC_VECTOR (15 downto 0);
+           output :    out  STD_LOGIC);
+end entity;
+
+architecture arch_piso of ShifterRegisterPISO16 is
+
+signal sr: std_logic_vector(15 downto 0) := (others=>'0');
+
+begin
+
+  process (clock,shift,input)
+  begin
+    if (shift = '0') then
+      sr <= input;
+    elsif rising_edge(clock) then
+      sr <= '0' & sr(15 downto 1);
+    end if;
+  end process;
+
+  output <= sr(0);      
+ 
+end architecture;
