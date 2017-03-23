f5e9db4fea09786ccbed3b6454af824a66abcc0e
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-23 12:36:44 -0300

    mais um teste do piso16 invertendo a direção

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
@@ -10,19 +10,20 @@ end entity;
 
 architecture arch_piso of ShiftRegisterPISO16 is
 
-signal sr: std_logic_vector(15 downto 0) := (others=>'0');
+signal w1: std_logic_vector(15 downto 0) := (others=>'0');
 
 begin
 
   process (clock,shift,input)
+  
   begin
     if (shift = '0') then
-      sr <= input;
+      w1 <= input;
     elsif rising_edge(clock) then
-      sr <= '0' & sr(15 downto 1);
+      w1 <= '0' & w1(15 to 1);
     end if;
   end process;
 
-  output <= sr(0);      
+  output <= w1(0);      
  
 end architecture;
