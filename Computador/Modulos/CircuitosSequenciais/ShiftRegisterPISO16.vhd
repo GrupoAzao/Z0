1f11b07d0706cd2313ac46640fd0d54c6d51dffb
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-03-23 01:17:26 -0300

    mais um teste do PISO16

--- Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
+++ Computador/Modulos/CircuitosSequenciais/ShiftRegisterPISO16.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ShiftRegisterPISO16 is
     Port ( clock :  in   STD_LOGIC;
-           load : in   STD_LOGIC;
+           shift: in   STD_LOGIC;
            input :    in   STD_LOGIC_VECTOR (15 downto 0);
            output :    out  STD_LOGIC);
 end entity;
@@ -14,15 +14,14 @@ architecture arch_piso of ShiftRegisterPISO16 is
 
 begin
 
-  process (clock,load,input)
+  process (clock,shift,input)
   begin
-    if (load = '1') then
+    if (shift = '1') then
       w1 <= input;
     elsif rising_edge(clock) then
       w1 <= '0' & w1(15 downto 1);
     end if;
   end process;
-
   output <= w1(0);      
  
 end architecture;
