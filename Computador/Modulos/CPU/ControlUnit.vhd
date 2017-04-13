ed66d8852f88410677537631cd324fc3da07e41e
guizg <guizgraicer@gmail.com>
2017-04-13 15:41:31 -0300

    first try

--- Computador/Modulos/CPU/ControlUnit.vhd
+++ Computador/Modulos/CPU/ControlUnit.vhd
@@ -14,8 +14,29 @@ entity ControlUnit is
 		loadA, loadD, loadM, loadPC : out STD_LOGIC                      -- sinais de load do reg. A, reg. D, Mem. RAM e Program Counter
 		
 		
-		
-		
-		
     );
 end entity;
+
+architecture CU of ControlUnit is
+
+begin	
+
+muxALUI_A <= !instruction(0);
+loadA <= !instruction(0);
+muxAM_ALU <= instruction(3) and instruction(0);
+
+zx <= instruction(4) and instruction(0);
+nx <= instruction(5) and instruction(0);
+zy <= instruction(6) and instruction(0);
+ny <= instruction(7) and instruction(0);
+f <= instruction(8) and instruction(0);
+no <= instruction(9) and instruction(0);
+
+loadA <= instruction(10) or !instruction(0);
+loadD <= instruction(11) and instruction(0);
+loadM <= instruction(12) and instruction(0);
+
+loadPC <= (instruction(15) and !ng and !zr) or (instruction(14) and zr) or (instruction(13) and ng and !zr);
+	
+end architecture;
+	
