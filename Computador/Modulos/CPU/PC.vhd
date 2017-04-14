24696213d463f4a6de2bd494f6f49fdce6aedae1
vilela69 <frederico.vilelacurti@gmail.com>
2017-04-13 23:07:04 -0300

    mais uma tentativa

--- Computador/Modulos/CPU/PC.vhd
+++ Computador/Modulos/CPU/PC.vhd
@@ -24,6 +24,7 @@ begin
 
 PC_process : process(clock,increment,load,reset,input)
   variable last_output : STD_LOGIC_VECTOR(14 downto 0);
+  variable incbyte : STD_LOGIC_VECTOR(14 downto 0);
 begin
   if clock'event and clock ='1' then
 
@@ -36,17 +37,18 @@ begin
       last_output := "000000000000000";
 
     elsif load = '1' and increment = '1' then
-      output <= STD_LOGIC_VECTOR(unsigned(input) +1);
-      last_output := STD_LOGIC_VECTOR(unsigned(input) +1);
+      incbyte := STD_LOGIC_VECTOR(unsigned(input) +1);
+      output <= incbyte;
+      last_output := incbyte;
 
     elsif load = '1' then
       output <= input(14 downto 0);
       last_output := input(14 downto 0);
 
-
     elsif increment = '1' then
-      output <= STD_LOGIC_VECTOR(unsigned(last_output) + 1);
-      last_output := STD_LOGIC_VECTOR(unsigned(last_output) + 1);
+      incbyte := STD_LOGIC_VECTOR(unsigned(last_output) + 1);
+      output <= incbyte;
+      last_output := incbyte;
 
     else
       output <= last_output;
