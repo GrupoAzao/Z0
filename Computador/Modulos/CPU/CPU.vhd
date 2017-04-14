1e776c8f622c7593a6874f05926e04632a650029
hugoeflavia <hugo.mendessp@yahoo.com.br>
2017-04-13 22:38:27 -0300

    debug

--- Computador/Modulos/CPU/CPU.vhd
+++ Computador/Modulos/CPU/CPU.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
@@ -14,4 +13,70 @@ entity CPU is
         pcout:       out STD_LOGIC_VECTOR(14 downto 0)     -- endereço para ser enviado a memória ROM
   );
 end entity;
+architecture CPU_arch of CPU is
+signal w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w18,w19 : STD_LOGIC;
+signal w13,w14,w15,w16,w17,w20 : STD_LOGIC_VECTOR(15 downto 0);
+component ControlUnit
+  port(
+		instruction                 : in STD_LOGIC_VECTOR(15 downto 0);  -- instrução para executar
+		zr,ng                       : in STD_LOGIC;                      -- valores zr(se zero) e ng(se negativo) da ALU
+		muxALUI_A                   : out STD_LOGIC;                     -- mux que seleciona entre instrução e ALU para reg. A
+		muxAM_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
+		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
+		loadA, loadD, loadM, loadPC : out STD_LOGIC                      -- sinais de load do reg. A, reg. D, Mem. RAM e Program Counter
+    );
+end component;
+component Register16
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(15 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(15 downto 0)
+	);
+end component;
 
+component Mux16
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			sel: in  STD_LOGIC;
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end component;
+
+component ALU
+	port ( 
+			x,y:   in STD_LOGIC_VECTOR(15 downto 0); -- entradas de dados da ALU
+			zx:    in STD_LOGIC;                     -- zera a entrada x
+			nx:    in STD_LOGIC;                     -- inverte a entrada x
+			zy:    in STD_LOGIC;                     -- zera a entrada y
+			ny:    in STD_LOGIC;                     -- inverte a entrada y
+			f:     in STD_LOGIC;                     -- se 0 calcula x & y, senão x + y
+			no:    in STD_LOGIC;                     -- inverte o valor da saída
+			zr:    out STD_LOGIC;                    -- setado se saída igual a zero
+			ng:    out STD_LOGIC;                    -- setado se saída é negativa
+			saida: out STD_LOGIC_VECTOR(15 downto 0) -- saída de dados da ALU
+	); 
+end component;
+
+component PC
+    port(
+        clock     : in  STD_LOGIC;
+		increment : in  STD_LOGIC;
+		load      : in  STD_LOGIC;
+		reset     : in  STD_LOGIC;
+        input     : in  STD_LOGIC_VECTOR(15 downto 0);
+        output    : out STD_LOGIC_VECTOR(14 downto 0)
+    );
+end component;
+begin
+	e1: ControlUnit port map (instruction,w18,w19,w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12);
+	e2 : Mux16 port map (w13,instruction,w1,w14);
+	e3 : Register16 port map (clock,w14,w9,w15);
+	e4 : Mux16 port map (w15,inM,w2,w16);
+	AddressM <= w15(14 downto 0);
+	e5 : ALU port map (w17,w16,w3,w4,w5,w6,w6,w8,w18,w19,w13);
+	outM <= w20;
+	e6: Register16 port map (clock,w13,w10,w17);
+	e7: PC port map (clock,'1',w12,reset,w15,pcout);
+	writeM <= w11;
+end architecture;
