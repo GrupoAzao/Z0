edd60880f042e800d36a0573ebdd42338e601407
eduardotp1 <edu.tirta@gmail.com>
2017-05-03 20:22:16 -0300

    symboltable pronto

--- Codigos/AssemblerZ0/src/main/java/assembler/SymbolTable.java
+++ Codigos/AssemblerZ0/src/main/java/assembler/SymbolTable.java
@@ -5,6 +5,8 @@
 
 package assembler;
 
+import java.util.Hashtable;
+
 /**
  * Mantém uma tabela com a correspondência entre os rótulos simbólicos e endereços numéricos de memória.
  */
@@ -13,8 +15,34 @@ public class SymbolTable {
     /**
      * Cria a tabela de símbolos.
      */
+	Hashtable<String, Integer> staticSymbols;
     public SymbolTable() {
-
+		staticSymbols = new Hashtable<String, Integer>();
+		
+	   staticSymbols.put("R0", 0);
+	   staticSymbols.put("R1", 1);
+	   staticSymbols.put("R2", 2);
+	   staticSymbols.put("R3", 3);
+	   staticSymbols.put("R4", 4);
+	   staticSymbols.put("R5", 5);
+	   staticSymbols.put("R6", 6);
+	   staticSymbols.put("R7", 7);
+	   staticSymbols.put("R8", 8);
+	   staticSymbols.put("R9", 9);
+	   staticSymbols.put("R10", 10);
+	   staticSymbols.put("R11", 11);
+	   staticSymbols.put("R12", 12);
+	   staticSymbols.put("R13", 13);
+	   staticSymbols.put("R14", 14);
+	   staticSymbols.put("R15", 15);
+	   staticSymbols.put("SP", 0);
+	   staticSymbols.put("LCL", 1);
+	   staticSymbols.put("ARG", 2);
+	   staticSymbols.put("THIS", 3);
+	   staticSymbols.put("THAT", 4);
+	   staticSymbols.put("SCREEN", 16384);
+	   staticSymbols.put("KBD", 24576 );
+    	   
     }
 
     /**
@@ -22,7 +50,8 @@ public class SymbolTable {
      * @param  symbol símbolo a ser armazenado na tabela de símbolos.
      * @param  address símbolo a ser armazenado na tabela de símbolos.
      */
-    public void addEntry(String symbol, int address) {
+    public void addEntry(String symbol, Integer address) {
+    	staticSymbols.put(symbol,address);
 
     }
 
@@ -31,8 +60,15 @@ public class SymbolTable {
      * @param  symbol símbolo a ser procurado na tabela de símbolos.
      * @return Verdadeiro se símbolo está na tabela de símbolos, Falso se não está na tabela de símbolos.
      */
-    public Boolean contains(String symbol) {
-    	return null;
+    public boolean contains(String symbol) {
+    	if (staticSymbols.containsKey(symbol)){
+    		return true;
+    	}
+    	else{
+    		return false;
+    	}
+    	
+
     }
 
     /**
@@ -40,8 +76,8 @@ public class SymbolTable {
      * @param  symbol símbolo a ser procurado na tabela de símbolos.
      * @return valor numérico associado ao símbolo procurado.
      */
-    public Integer getAddress(String symbol) {
-    	return null;
+    public int getAddress(String symbol) {
+    	return staticSymbols.get(symbol);
     }
 
 }
