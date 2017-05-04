bac1092f4090986a33f147409ccb60dcf7fadfe0
guizg <guizgraicer@gmail.com>
2017-05-04 00:13:25 -0300

    pronto

--- Codigos/AssemblerZ0/src/main/java/assembler/Parser.java
+++ Codigos/AssemblerZ0/src/main/java/assembler/Parser.java
@@ -5,13 +5,23 @@
 
 package assembler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Encapsula o código de leitura. Carrega as instruções na linguagem assembly,
  * analisa, e oferece acesso as partes da instrução  (campos e símbolos).
  * Além disso, remove todos os espaços em branco e comentários.
  */
 public class Parser {
-
+	private String text;
+	private SymbolTable st;
+	private int Counter;
+	private String line;
+	private StringBuilder sb;
+	private BufferedReader br;
+	
     /** Enumerator para os tipos de comandos do Assembler. */
     public enum CommandType {
         A_COMMAND,      // comandos LEA, que armazenam no registrador A
@@ -22,26 +32,68 @@ public class Parser {
     /** 
      * Abre o arquivo de entrada NASM e se prepara para analisá-lo.
      * @param file arquivo NASM que será feito o parser.
+     * @throws IOException 
      */
-    public Parser(String file) {
+    public Parser(String file) throws IOException {
+        br = new BufferedReader(new FileReader(file));
+    	st = new SymbolTable();
+    	try {
+    		sb = new StringBuilder();
+    	   	line = command();
+    	    do {
+    	    	Counter +=1;
+    	    	int coment =  line.indexOf(';');
+    	    	if(coment > 0){
+    	    		line = line.substring(0, coment-1);
+    	    		sb.append(line);
 
+    	    	}else if (coment == -1) {
+    	    		if (line.trim().length() > 0){
+    	    			sb.append(line);
+    	    			
+    	    		}
+    	    	}
+    	    	if (line.contains(":")){
+    	    		st.addEntry(this.label(line), Counter);
+    	    	}
+    	    }  while (advance());
+    	    
+    	    String everything = sb.toString();
+    	    this.text = everything;
+    	    
+    	} finally {
+    	    br.close();
+    	}
     }
+    
 
     /**
      * Carrega uma instrução e avança seu apontador interno para o próxima
      * linha do arquivo de entrada. Caso não haja mais linhas no arquivo de
      * entrada o método retorna "Falso", senão retorna "Verdadeiro".
      * @return Verdadeiro se ainda há instruções, Falso se as instruções terminaram.
+     * @throws IOException 
      */
-    public boolean advance() {
+
+    public boolean advance() throws IOException {
+    	sb.append(System.lineSeparator());
+		line = br.readLine();
+		if(line != null){
+			return true;
+		}else{
+			return false;
+		}
 
     }
 
     /**
      * Retorna o comando "intrução" atual (sem o avanço)
      * @return a instrução atual para ser analilisada
+     * @throws IOException 
      */
-    public String command() {
+
+    public String command() throws IOException {
+    	return br.readLine();
 
     }
 
@@ -54,6 +106,7 @@ public class Parser {
      * @return o tipo da instrução.
      */
     public CommandType commandType(String command) {
+
     	Character first = command.charAt(0); // pega a primeira letra
     	
     	if(first == 'l'){ // se ela for 'l' a instrucao eh leaw
@@ -75,11 +128,15 @@ public class Parser {
      * Deve ser chamado somente quando commandType() é A_COMMAND.
      * @param  command instrução a ser analisada.
      * @return somente o símbolo ou o valor número da instrução.
+     * leaw $51,%A
      */
     public String symbol(String command) {
     	String[] mnemos = command.split("\\s");
-    	mnemos[1].replace("$", "");
-    	return mnemos[1];
+    	String a1 = mnemos[1].replace("$", "");
+    	a1 = a1.replace(",%A", "");
+    	System.out.println(a1);
+    	return a1;
+
     }
 
     /**
@@ -101,12 +158,13 @@ public class Parser {
      * @return um vetor de string contento os tokens da instrução (as partes do comando).
      */
     public String[] instruction(String command) {
-    	String[] mnemos = command.split("\\s");
-    	for(String m:mnemos){
-    		m.replace(",", "");
-    		m.replace(";", "");
-    	}
+    	String[] mnemos = new String[3];
+    	mnemos[0]= command.split("\\s")[0];
+    	String temp =command.split("\\s")[1];
+    	mnemos[1]  = temp.split(",")[0];
+    	mnemos[2] = temp.split(",")[1];   	
     	return mnemos;
+
     }
 
 }
