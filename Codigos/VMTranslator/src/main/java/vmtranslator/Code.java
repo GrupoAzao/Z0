6bced2a4412605dbba61402472a327cfbf25670f
Martim <martim.ferreira.jose@gmail.com>
2017-05-11 09:14:09 -0300

    Alguns feitos e push pop só com a estrutura

--- Codigos/VMTranslator/src/main/java/vmtranslator/Code.java
+++ Codigos/VMTranslator/src/main/java/vmtranslator/Code.java
@@ -2,95 +2,154 @@
  * Curso: Elementos de Sistemas
  * Arquivo: Code.java
  */
-
+writepushpop, writelabel, writegoto, writeif
 package vmtranslator;
 
 /** 
- * Traduz da linguagem vm para códigos assembly.
+ * Traduz da linguagem vm para cÃ³digos assembly.
  */
 public class Code {
 
     /** 
-     * Abre o arquivo de entrada VM e se prepara para analisá-lo.
-     * @param filename nome do arquivo VM que será feito o parser.
+     * Abre o arquivo de entrada VM e se prepara para analisÃ¡-lo.
+     * @param filename nome do arquivo VM que serÃ¡ feito o parser.
      */
     public Code(String filename) {
 
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para executar o comando aritmético.
-     * @param  command comando aritmético a ser analisado.
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para executar o comando aritmÃ©tico.
+     * @param  command comando aritmÃ©tico a ser analisado.
      */
     public void writeArithmetic(String command) {
+    	if(command.equals("add")){
+    		
+    	}
+    	
+    	else if(command.equals("sub")){
+    		
+    	}
+    	
+    	else if(command.equals("neg")){
+    		
+    	}
+    	
+    	else if(command.equals("eq")){
+    		
+    	}
+    	
+    	else if(command.equals("gt")){
+    		
+    	}
+    	
+    	else if(command.equals("lt")){
+    		
+    	}
+    	
+    	else if(command.equals("and")){
+    		
+    	}
+    	
+    	else if(command.equals("or")){
+    		
+    	}
+    	
+    	else if(command.equals("not")){
+    		
+    	}
+    	
+	}
 
-    }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para executar o comando de Push ou Pop.
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para executar o comando de Push ou Pop.
      * @param  command comando de push ou pop a ser analisado.
-     * @param  segment segmento de memória a ser usado pelo comando.
-     * @param  index índice do segkento de memória a ser usado pelo comando.
+     * @param  segment segmento de memÃ³ria a ser usado pelo comando.
+     * @param  index Ã­ndice do segkento de memÃ³ria a ser usado pelo comando.
      */
     public void writePushPop(Parser.CommandType command, String segment, Integer index) {
-
+    	if (segment.equals("local")){
+    		segment = "1";
+    	}
+    	if (segment.equals("argument")){
+    		segment = "2";
+    	}
+    	if (segment.equals("this")){
+    		segment = "3";
+    	}
+    	if (segment.equals("that")) {
+    		segment = "4";
+    	}
+    	
+    	
+    	if(command.equals(CommandType.C_PUSH)){
+    	
+    	}
+    	
+    	else if(command.equals(CommandType.C_POP)){
+    		
+    	}
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para inicializar o processo da VM (bootstrap).
-     * Também prepara a chamada para a função Sys.init
-     * O código deve ser colocado no início do arquivo de saída.
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para inicializar o processo da VM (bootstrap).
+     * TambÃ©m prepara a chamada para a funÃ§Ã£o Sys.init
+     * O cÃ³digo deve ser colocado no inÃ­cio do arquivo de saÃ­da.
      */
     public void writeInit() {
 
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para gerar o labels (marcadores de jump).
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para gerar o labels (marcadores de jump).
      * @param  label define nome do label (marcador) a ser escrito.
      */
     public void writeLabel(String label) {
-
+    	writer.write(label + ":");
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para gerar as instruções de goto (jumps).
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para gerar as instruÃ§Ãµes de goto (jumps).
      * Realiza um jump incondicional para o label informado.
      * @param  label define jump a ser realizado para um label (marcador).
      */
     public void writeGoto(String label) {
-
+    	writer.write("leaw %" + label + ", %A");
+    	writer.write("jne"); //Faz um jump para o endereço armazenado em A
+    	writer.write("nop");
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para gerar as instruções de goto condicional (jumps condicionais).
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para gerar as instruÃ§Ãµes de goto condicional (jumps condicionais).
      * Realiza um jump condicional para o label informado.
      * @param  label define jump a ser realizado para um label (marcador).
      */
     public void writeIf(String label) {
-
+    	writer.write("leaw %" + label + ", %A");
+    	writer.write("jne"); //Faz um jump para o endereço armazenado em A
+    	writer.write("nop");
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para uma chamada de função (Call).
-     * @param  functionName nome da função a ser "chamada" pelo call.
-     * @param  numArgs número de argumentos a serem passados na função call.
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para uma chamada de funÃ§Ã£o (Call).
+     * @param  functionName nome da funÃ§Ã£o a ser "chamada" pelo call.
+     * @param  numArgs nÃºmero de argumentos a serem passados na funÃ§Ã£o call.
      */
     public void writeCall(String functionName, Integer numArgs) {
-
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para o retorno de uma sub rotina.
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para o retorno de uma sub rotina.
      */
     public void writeReturn() {
 
     }
 
     /**
-     * Grava no arquivo de saida as instruções em Assembly para a declaração de uma função.
-     * @param  functionName nome da função a ser criada.
-     * @param  numLocals número de argumentos a serem passados na função call.
+     * Grava no arquivo de saida as instruÃ§Ãµes em Assembly para a declaraÃ§Ã£o de uma funÃ§Ã£o.
+     * @param  functionName nome da funÃ§Ã£o a ser criada.
+     * @param  numLocals nÃºmero de argumentos a serem passados na funÃ§Ã£o call.
      */
     public void writeFunction(String functionName, Integer numLocals) {
 
@@ -98,7 +157,7 @@ public class Code {
 
     /**
      * Armazena o nome do arquivo vm de origem.
-     * Usado para definir os dados estáticos do código (por arquivo).
+     * Usado para definir os dados estÃ¡ticos do cÃ³digo (por arquivo).
      * @param  filename nome do arquivo sendo tratado.
      */
     public void vmfile(String file) {
